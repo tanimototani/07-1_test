9542a6174dbe281b22874ece827f310dc62e3956
tanimototani <tatamimoti@icloud.com>
2023-03-03 15:18:07 +0900

    入力チェック

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 use App\Models\Post;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
@@ -22,4 +21,13 @@ class PostController extends Controller
    {
      return view('posts/create');
    }
+   
+   public function store(Post $post, PostRequest $request)
+   {
+      $input = $request['post'];
+      $post->fill($input)->save();
+      return redirect('/posts/' . $post->id);
+   }
+   
+   
 }
