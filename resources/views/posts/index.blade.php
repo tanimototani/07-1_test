51cc245d127157b38e2d5862cdc0026e87915d3b
tanimototani <tatamimoti@icloud.com>
2023-03-09 17:00:27 +0900

    削除ボタン

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -18,6 +18,11 @@
                  <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
              </h2>
              <p class='body'>{{ $post->body }}</p>
+             <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                 @csrf
+                 @method('DELETE')
+                 <button type="button" onclick="deletePost({{ $post->id }})">delete</button>
+             </form>
            </div>
           @endforeach
         </div>
@@ -25,4 +30,13 @@
             {{ $posts->links() }}
         </div>
     </body>
+    <script>
+        function deletePost(id) {
+            'use strict'
+            
+            if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
+                document.getElementById(`form_${id}`).submit();
+            }
+        }
+    </script>
 </html>
